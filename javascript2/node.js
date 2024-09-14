ca97b7de0f10dddfa75a7ba91c5f87a201a440dc
mahad7-prog <mahadali245.a@gmail.com>
2024-09-14 13:21:19 +0500

    Add files via upload

---
+++ javascript2/node.js
@@ -0,0 +1,69 @@
+var input = prompt("enter any number")
+document.write("<h1>" + "Result:" + "</h1>" + "</br>")
+document.write("The value of a is:" + input + "</br>")
+document.write("********************************" + "</br>"+ "</br>"+ "</br>")
+ ++input
+ document.write("The value of ++a is:" + input + "</br>")
+ document.write("Now the value of a is:" + input + "</br>"+ "</br>"+ "</br>")
+
+ document.write("The value of a++ is:" + input + "</br>")
+ input++
+ document.write("Now the value of a is:" + input + "</br>"+ "</br>"+ "</br>")
+ --input
+ document.write("The value of --a is:" + input + "</br>")
+ document.write("Now the value of a is:" + input + "</br>"+ "</br>"+ "</br>")
+ 
+ document.write("The value of a-- is:" + input + "</br>")
+ input--
+ document.write("Now the value of a is:" + input + "</br>"+ "</br>"+ "</br>")
+ 
+
+
+
+//  var a = 2 , b = 1
+//  var result =  --a - --b + ++b + b--; 
+//  //1 - 0 + 1 + 1
+//  document.write("value of a =" + a + "</br>")
+//  document.write("Value of b =" + b + "</br>")
+//  document.write("The result is =" + result + "</br>"+ "</br>"+ "</br>")
+
+//  var name = prompt("enter your name")
+//  alert("Welcome " + name)
+
+// var number = +prompt("enter number")
+// if(number == 0){
+//  document.write("5 X 1 = 5" + "</br>")
+//  document.write("5 X 2 = 10" + "</br>")
+//  document.write("5 X 3 = 15" + "</br>")
+//  document.write("5 X 4 = 20" + "</br>")
+//  document.write("5 X 5 = 25" + "</br>")
+//  document.write("5 X 6 = 30" + "</br>")
+//  document.write("5 X 7 = 35" + "</br>")
+//  document.write("5 X 8 = 40" + "</br>")
+//  document.write("5 X 9 = 45" + "</br>")
+//  document.write("5 X 10 = 50" + "</br>")
+// }else{
+//     document.write(number + " X 1 =" + number*1 + "</br>")
+//     document.write(number + " X 2 =" + number*2 + "</br>")
+//     document.write(number + " X 3 =" + number*3 + "</br>")
+//     document.write(number + " X 4 =" + number*4 + "</br>")
+//     document.write(number + " X 5 =" + number*5 + "</br>")
+//     document.write(number + " X 6 =" + number*6 + "</br>")
+//     document.write(number + " X 7 =" + number*7 + "</br>")
+//     document.write(number + " X 8 =" + number*8 + "</br>")
+//     document.write(number + " X 9 =" + number*9 + "</br>")
+//     document.write(number + " X 10 =" + number*10 + "</br>")
+// }
+
+
+// var subject1 = prompt("enter subject name")
+// var subject2 = prompt("enter subject name")
+// var subject3 = prompt("enter subject name")
+// var maxmarks = 100
+// var obtained1 = +prompt("enter " + subject1 + " number")
+// var obtained2 = +prompt("enter " + subject2 + " number")
+// var obtained3 = +prompt("enter " + subject3 + " number")
+// var totalmarks = obtained1 + obtained2 + obtained3
+// var percentage = (totalmarks/300)*100
+// document.write("Subject Total-Marks Obtained-Marks Percentage" + "</br>")
+// document.write(subject1 + maxmarks + obtained1 +   percentage)
